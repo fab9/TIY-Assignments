23cc2379416ba30c9a6f7d3a2e8de7ba9d225792
fab9 <rorou192@gmail.com>
2014-10-07 19:47:13 -0400

    first failing test

--- 2014-10-01-conway-function.js
+++ 2014-10-01-conway-function.js
@@ -1,3 +1,16 @@
-Implement a simple function named conway(cell, neighbors) that...
-Given the state of its neighbors
-Returns the state of cell after 1 tick
+/**
+ * Conway
+ *
+ * GIVEN: a cell and an array made up of the state of its neighbors
+ * WHEN: called with `conway(cell, neighbors)`
+ * THEN: Returns the state of cell after 1 tick
+ */
+/** === PRODUCTION CODE === **/
+function conway(cell, neighbors) {
+
+}
+
+
+/** === TEST CODE === **/
+console.log('It should return state of a cell after 1 tick: ',
+  conway(cell, neighbors));
