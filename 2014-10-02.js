b892f65e630c2aaf0e12416059d8a4b8a09a0cf3
fab9 <rorou192@gmail.com>
2014-10-08 10:17:25 -0400

    describe block is not working
    I wonder if I'm missing necessary mocha code fo it to run, or if the test is legitimately failing.
    
    Please enter the commit message for your changes. Lines starting

--- 2014-10-02.js
+++ 2014-10-02.js
@@ -1,19 +1,5 @@
-var assert = require('assert');
+var assert = require('assert')
 
-/**
- * Log `success` if `actual` is STRICTLY equal to `expected`
- *
- * @param ANY actual
- * @param ANY expected
- * @param String success
- */
-function test(actual, expected, success){
-    if (success === undefined) success = 'pass!';
-
-    assert.strictEqual(actual, expected);
-
-    console.log(success);
-}
 /** === PRODUCTION CODE === **/
 /**
  * Return a nested array representing an initially empty 3x3 board.
@@ -55,68 +41,68 @@ function conway(cell, neighbors) {
  * THEN returns a list of the neighboring cells.
  *
  */
- function neighborsOf(board, row, col) {
-  var neighbors = [];
-   if (row === 0 && col === 0) {
-        return [
-          board[0][1], board[1][0], board[1][1]
-        ];
-    }
+//  function neighborsOf(board, row, col) {
+//   var neighbors = [];
+//    if (row === 0 && col === 0) {
+//         return [
+//           board[0][1], board[1][0], board[1][1]
+//         ];
+//     }
 
-    if (row === 0 && col === 1) {
-        return [
-          board[0][0], board[1][0], board[1][1],
-          board[0][2], board[1][2]
-        ];
-    }
+//     if (row === 0 && col === 1) {
+//         return [
+//           board[0][0], board[1][0], board[1][1],
+//           board[0][2], board[1][2]
+//         ];
+//     }
 
-    if (row === 0 && col === 2) {
-        return [
-          board[0][1], board[1][1], board[1][2]
-        ];
-    }
+//     if (row === 0 && col === 2) {
+//         return [
+//           board[0][1], board[1][1], board[1][2]
+//         ];
+//     }
 
-    if (row === 1 && col === 0) {
-        return [
-          board[0][0], board[0][1], board[1][1],
-          board[2][0], board[2][1]
-        ];
-    }
+//     if (row === 1 && col === 0) {
+//         return [
+//           board[0][0], board[0][1], board[1][1],
+//           board[2][0], board[2][1]
+//         ];
+//     }
 
-    if (row === 1 && col === 1) {
-        return [
-          board[0][0], board[0][1], board[0][2],
-          board[1][0], board[1][2], board[2][0],
-          board[2][1], board[2][2]
-        ];
-    }
+//     if (row === 1 && col === 1) {
+//         return [
+//           board[0][0], board[0][1], board[0][2],
+//           board[1][0], board[1][2], board[2][0],
+//           board[2][1], board[2][2]
+//         ];
+//     }
 
-    if (row === 1 && col === 2) {
-        return [
-          board[0][1], board[0][2], board[1][1],
-          board[2][1], board[2][2]
-        ];
-    }
+//     if (row === 1 && col === 2) {
+//         return [
+//           board[0][1], board[0][2], board[1][1],
+//           board[2][1], board[2][2]
+//         ];
+//     }
 
-    if (row === 2 && col === 0) {
-        return [
-          board[1][0], board[1][1], board[2][1]
-        ];
-    }
+//     if (row === 2 && col === 0) {
+//         return [
+//           board[1][0], board[1][1], board[2][1]
+//         ];
+//     }
 
-    if (row === 2 && col === 1) {
-        return [
-          board[1][0], board[2][0], board[1][1],
-          board[1][2], board[2][2]
-        ];
-    }
+//     if (row === 2 && col === 1) {
+//         return [
+//           board[1][0], board[2][0], board[1][1],
+//           board[1][2], board[2][2]
+//         ];
+//     }
 
-    if (row === 2 && col === 2) {
-        return [
-          board[1][1], board[1][2], board[2][1]
-        ];
-    }
-} // END neighborsOf
+//     if (row === 2 && col === 2) {
+//         return [
+//           board[1][1], board[1][2], board[2][1]
+//         ];
+//     }
+// } // END neighborsOf
 
  /**
   * Function Definition: tick()
@@ -137,24 +123,24 @@ function conway(cell, neighbors) {
  *
  */
 // Concatenate all 3 arrays
-var row0 = board[0]
-var row1 = board[1]
-var row2 = board[2]
-var flatArray = row0.concat(row1, row2);
+// var row0 = board[0]
+// var row1 = board[1]
+// var row2 = board[2]
+// var flatArray = row0.concat(row1, row2);
 
 // Give me the total number of live cells
-function checkForAliveCells(array) {
-  var numberAliveCounter = 0;
-  for (var i = 0; i < array.length; i++) {
-      if (array[i] === true) {
-          numberAliveCounter++
-      };
-  };
-  return numberAliveCounter;
-}
+// function checkForAliveCells(array) {
+//   var numberAliveCounter = 0;
+//   for (var i = 0; i < array.length; i++) {
+//       if (array[i] === true) {
+//           numberAliveCounter++
+//       };
+//   };
+//   return numberAliveCounter;
+// }
 
-console.log('it should check that board is empty: ',
-  checkForAliveCells(flatArray) === 0);
+// console.log('it should check that board is empty: ',
+//   checkForAliveCells(flatArray) === 0);
 
 // David's rules tests
 describe('generation rule', function() {
