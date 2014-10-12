10f213387446ef83bbfc27d70dd32cdf77ed2756
fab9 <rorou192@gmail.com>
2014-10-12 18:50:02 -0400

    WIP

--- 2014-10-02.js
+++ 2014-10-02.js
@@ -1,4 +1,14 @@
-var assert = require('assert')
+var assert = require('assert');
+/**
+ * Log `success` if `actual` is STRICTLY equal to `expected`
+ *
+ * @param ANY actual
+ * @param ANY expected
+ * @param String success
+ */
+function test(actual, expected){
+    assert.strictEqual(actual, expected);
+}
 
 /** === PRODUCTION CODE === **/
 /**
@@ -6,11 +16,13 @@ var assert = require('assert')
  *
  * @return Array of Array of Boolean
  */
-var board = [
-    [ false, false, false ],
-    [ false, false, false ],
-    [ false, false, false ],
+function board(){
+    return [
+        [ false, false, false ],
+        [ false, false, false ],
+        [ false, false, false ],
     ];
+}
 
 /**
  * Function Definition: conway()
@@ -23,86 +35,107 @@ var board = [
  * THEN returns the state of the `cell` with the rules applied.
  *
  */
-function conway(cell, neighbors) {
-    if (neighbors.length === 3) {
+function conway(cell, neighbors){
+    var alive = 0;
+    neighbors.forEach(function(neighbor){
+        if ( neighbor ){
+            alive++;
+        }
+    });
+
+    if ( cell && alive === 2 ){
+        return true;
+    }
+    if ( alive === 3 ){
         return true;
     }
     return false;
-}
+}// END conway
 
-} // END conway
+/** === DAVID'S TESTS CODE FOR CONWAY() === **/
+describe('generation rule', function() {
+
+  it('should NOT generate a new cell if there are fewer than 3 neighbors', function() {
+      test(conway(false, []), false);
+      test(conway(false,[true]), false);
+      test(conway(false, [true, true]), false);
+      test(conway(false, [false, false, false]), false);
+  });
+
+  it('should generate a new cell if there are exactly 3 neighbors', function() {
+    test(conway(false, [true, true, true]), true);
+  });
+})
+
 
 /**
- * Function Definition: neighborsOf()
- *
  * @name neighborsOf Identifier
- * TODO: add all necessary info to this block
  * GIVEN a `board` and `row` and `col` coordinates...
  * THEN returns a list of the neighboring cells.
  *
  */
-//  function neighborsOf(board, row, col) {
-//   var neighbors = [];
-//    if (row === 0 && col === 0) {
-//         return [
-//           board[0][1], board[1][0], board[1][1]
-//         ];
-//     }
+ function neighborsOf(board, row, col) {
+  var neighbors = [];
+   if (row === 0 && col === 0) {
+        return [
+          board[0][1], board[1][0], board[1][1]
+        ];
+    }
 
-//     if (row === 0 && col === 1) {
-//         return [
-//           board[0][0], board[1][0], board[1][1],
-//           board[0][2], board[1][2]
-//         ];
-//     }
+    if (row === 0 && col === 1) {
+        return [
+          board[0][0], board[1][0], board[1][1],
+          board[0][2], board[1][2]
+        ];
+    }
 
-//     if (row === 0 && col === 2) {
-//         return [
-//           board[0][1], board[1][1], board[1][2]
-//         ];
-//     }
+    if (row === 0 && col === 2) {
+        return [
+          board[0][1], board[1][1], board[1][2]
+        ];
+    }
 
-//     if (row === 1 && col === 0) {
-//         return [
-//           board[0][0], board[0][1], board[1][1],
-//           board[2][0], board[2][1]
-//         ];
-//     }
+    if (row === 1 && col === 0) {
+        return [
+          board[0][0], board[0][1], board[1][1],
+          board[2][0], board[2][1]
+        ];
+    }
 
-//     if (row === 1 && col === 1) {
-//         return [
-//           board[0][0], board[0][1], board[0][2],
-//           board[1][0], board[1][2], board[2][0],
-//           board[2][1], board[2][2]
-//         ];
-//     }
+    if (row === 1 && col === 1) {
+        return [
+          board[0][0], board[0][1], board[0][2],
+          board[1][0], board[1][2], board[2][0],
+          board[2][1], board[2][2]
+        ];
+    }
 
-//     if (row === 1 && col === 2) {
-//         return [
-//           board[0][1], board[0][2], board[1][1],
-//           board[2][1], board[2][2]
-//         ];
-//     }
+    if (row === 1 && col === 2) {
+        return [
+          board[0][1], board[0][2], board[1][1],
+          board[2][1], board[2][2]
+        ];
+    }
 
-//     if (row === 2 && col === 0) {
-//         return [
-//           board[1][0], board[1][1], board[2][1]
-//         ];
-//     }
+    if (row === 2 && col === 0) {
+        return [
+          board[1][0], board[1][1], board[2][1]
+        ];
+    }
 
-//     if (row === 2 && col === 1) {
-//         return [
-//           board[1][0], board[2][0], board[1][1],
-//           board[1][2], board[2][2]
-//         ];
-//     }
+    if (row === 2 && col === 1) {
+        return [
+          board[1][0], board[2][0], board[1][1],
+          board[1][2], board[2][2]
+        ];
+    }
 
-//     if (row === 2 && col === 2) {
-//         return [
-//           board[1][1], board[1][2], board[2][1]
-//         ];
-//     }
-// } // END neighborsOf
+    if (row === 2 && col === 2) {
+        return [
+          board[1][1], board[1][2], board[2][1]
+        ];
+    }
+} // END neighborsOf
 
  /**
   * Function Definition: tick()
@@ -118,45 +151,6 @@ function conway(cell, neighbors) {
 
 
 /** === TEST CODE === **/
-/**
- * Check starting position #1: Empty board
- *
- */
-// Concatenate all 3 arrays
-// var row0 = board[0]
-// var row1 = board[1]
-// var row2 = board[2]
-// var flatArray = row0.concat(row1, row2);
-
-// Give me the total number of live cells
-// function checkForAliveCells(array) {
-//   var numberAliveCounter = 0;
-//   for (var i = 0; i < array.length; i++) {
-//       if (array[i] === true) {
-//           numberAliveCounter++
-//       };
-//   };
-//   return numberAliveCounter;
-// }
-
-// console.log('it should check that board is empty: ',
-//   checkForAliveCells(flatArray) === 0);
-
-// David's rules tests
-describe('generation rule', function() {
-
-  it('should NOT generate a new cell if there are fewer than 3 neighbors', function() {
-      test(conway(false, []), false);
-      test(conway(false,[true]), false);
-      test(conway(false, [true, true]), false);
-      test(conway(false, [false, false, false]), false);
-  });
-
-  it('should generate a new cell if there are exactly 3 neighbors', function() {
-    test(conway(false, [true, true, true]), true);
-  });
-})
-
 
 
 
