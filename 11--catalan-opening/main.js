0ee313ab43c02ab7321ffac0392e62e99cc8ff6c
fab9 <rorou192@gmail.com>
2014-10-15 18:01:51 -0400

    @asoper @fab9 WIP

--- 11--catalan-opening/main.js
+++ 11--catalan-opening/main.js
@@ -90,13 +90,95 @@ var moves = {
 // game ends
 
 
+console.log(board.join('\n'));
 // ------- Tests
+
+var assert = require('chai').assert
+var expect = require('chai').expect
+
+
+
 describe('Testing the board state', function(){
-  it('Move 1', function(){
+  it('After Move 1, the board should look like:', function(){
+    moves.step1();
 
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K','B','N','R'],
+      ['P','P','P','P','P','P','P','P'],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      ['p','p','p',' ','p','p','p','p'],
+      ['r','n','b','q','k','b','n','r'] ]
+    )
+  });
+  it('after Move 2, the board should look like:', function(){
+    moves.step2();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K','B','N','R'],
+      ['P','P','P',' ','P','P','P','P'],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      ['p','p','p',' ','p','p','p','p'],
+      ['r','n','b','q','k','b','n','r'] ]
+    )
+  });
+  it('after Move 3, the board should look like:', function(){
+    moves.step3();
+
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K','B','N','R'],
+      ['P','P','P',' ','P','P','P','P'],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n',' ',' '],
+      ['p','p','p',' ','p','p','p','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    )
   })
+  it('after Move 4, the board should look like:', function(){
+    assert.deepEqual(board,
+      [
+      ['R','N','B','Q','K','B','N','R'],
+      ['P','P','P',' ',' ','P','P','P'],
+      [' ',' ',' ',' ','P',' ',' ',' '],
+      [' ',' ',' ','P',' ',' ',' ',' '],
+      [' ',' ',' ','p',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','n',' ',' '],
+      ['p','p','p',' ','p','p','p','p'],
+      ['r','n','b','q','k','b',' ','r'] ]
+    )
+  })
+  // it('should be a pawn at board[2][4]', function(){
+  //   assert.equal(moves.step7, board[2][4]);
+  // })
+  // it('should be a pawn at board[5][6]', function(){
+  //   assert.equal(moves.step9, board[5][6]);
+  // })
+  // it('should be a bishop at board[1][4]', function(){
+  //   assert.equal(moves.step11, board[1][4]);
+  // })
+  // it('after step 12: board[0][5] to be blank', function(){
+  //   expect(board[0][5]).to.equal(' ');
+  // })
+  // it('should be a pawn at board[4][2]', function(){
+  //   assert.equal(moves.step13, board[4][2]);
+  // })
+  // it('should be a knight at board[2][5]', function(){
+  //   assert.equal(moves.step15, board[2][5]);
+  // })
+  // it('should be a bishop at board[6][6]', function(){
+  //   assert.equal(moves.step17, board[6][6]);
+  // })
 })
-
 
 
 
