737a93190bbaa9a4464a4496db958e748985b256
fab9 <rorou192@gmail.com>
2014-10-15 19:26:35 -0400

    not working yet

--- 11--catalan-opening/main.js
+++ 11--catalan-opening/main.js
@@ -1,4 +1,5 @@
-var moves = [
+// Note: turns are commands, therefore we're not expecting it to return anything.
+var board = [
   ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'], // 0
   ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], // 1
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], // 2
@@ -9,96 +10,78 @@ var moves = [
   ['r', 'r', 'b', 'q', 'k', 'b', 'n', 'r'] // 7
  // 0,   1,   2,   3,   4,   5,   6,   7
 ];
-}
 
-// Note: turns are commands, therefore we're not expecting it to return anything.
-// Turn1: move pawn from   board[6][3] => board[4][3]
-//                         board[6][3] => ' ',
-//
+console.log(board.join('\n') + '\n\n');
+
 var moves = {
   // move1
   step1: function(){
-    board[4][3] = board[6][3];
-    board[6][3] = ' ';
+    board[4][4] = board[6][4];
+    board[6][4] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+
   },
 
-};
 
-  // move2
-  step2: function(){
-    board[0][6] = board[2][5];
-    board[0][6] = ' ';
-  },
-//
+//   // move2
+//   step2: function(){
+//     board[0][6] = board[2][5];
+//     board[0][6] = ' ';
+//   },
+// //
 
-  // move3
-  step3: function(){
-    board[6][4] = board[6][2];
-    board[6][2] = ' ';
-  },
-// Turn3: move pawn from board[6][2] => board[6][4]
-//                         board[6][2] => ' ',
-//
-  // move4
-  step4: function(){
-    board[2][4] = board[1][4];
-    board[1][4] = ' ';
-  },
-// Turn4: move pawn from board[1][4] => board[2][4]
-//                         board[1][4] => ' ',
+//   // move3
+//   step3: function(){
+//     board[6][4] = board[6][2];
+//     board[6][2] = ' ';
+//   },
 
-  // move5
-  step5: function(){
-    board[4][6] = board[6][6];
-    board[6][6] = ' ';
-  },
-// Turn5: move pawn from board[6][6] => board[4][6]
-//                         board[6][6] => ' ',
+//   // move4
+//   step4: function(){
+//     board[2][4] = board[1][4];
+//     board[1][4] = ' ';
+//   },
 
-  // move6
-  step6: function(){
-    board[3][3] = board[1][3];
-    board[1][3] = ' ';
-  },
-// Turn6: move pawn from board[1][3] => board[3][3]
-//                         board[1][3] => ' ',
+  // // move5
+  // step5: function(){
+  //   board[4][6] = board[6][6];
+  //   board[6][6] = ' ';
+  // },
 
-  // move7
-  step7: function(){
-    board[6][6] = board[7][5];
-    board[7][5] = ' ';
-  },
-// Turn7: move bishop from board[7][5] => board[6][6]
-//                         board[7][5] => ' ',
+  // // move6
+  // step6: function(){
+  //   board[3][3] = board[1][3];
+  //   board[1][3] = ' ';
+  // },
 
-  // move8
-  step8: function(){
-    board[1][4] = board[0][5];
-    board[0][5] = ' ';
-  },
-// Turn8: move bishop from board[0][5] => board[1][4]
-//                         board[0][5] => ' ',
+  // // move7
+  // step7: function(){
+  //   board[6][6] = board[7][5];
+  //   board[7][5] = ' ';
+  // },
 
-  // move9
-  step9: function(){
-    board[5][5] = board[7][6];
-    board[7][6] = ' ';
-  },
-// Turn9: move knight from board[7][6] => board[5][5]
-//                         board[7][6] => ' ',
+  // // move8
+  // step8: function(){
+  //   board[1][4] = board[0][5];
+  //   board[0][5] = ' ';
+  // },
+
+  // // move9
+  // step9: function(){
+  //   board[5][5] = board[7][6];
+  //   board[7][6] = ' ';
+  // },
 //
 // game ends
 }
 
-console.log(board.join('\n'));
 // ------- Tests
-
 var assert = require('chai').assert
 var expect = require('chai').expect
 
 
 describe('Testing the board state', function(){
-  it('After move 1, the board should look like:', function(){
+  it('after Move 1, the board should look like:', function(){
     moves.step1();
 
     assert.deepEqual(board,
@@ -144,6 +127,8 @@ describe('Testing the board state', function(){
     )
   });
   it('after Move 4, the board should look like:', function(){
+    moves.step4();
+
     assert.deepEqual(board,
       [
       ['R','N','B','Q','K','B','N','R'],
@@ -155,7 +140,7 @@ describe('Testing the board state', function(){
       ['p','p','p',' ','p','p','p','p'],
       ['r','n','b','q','k','b',' ','r'] ]
     )
-  })
+  });
   // it('should be a pawn at board[2][4]', function(){
   //   assert.equal(moves.step7, board[2][4]);
   // })
@@ -178,8 +163,4 @@ describe('Testing the board state', function(){
   //   assert.equal(moves.step17, board[6][6]);
   // })
 //
-
-
-
-
-
+ });
