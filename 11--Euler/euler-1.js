7e93bfdd634c226d35cffd91a5556b339b00299a
fab9 <rorou192@gmail.com>
2014-10-16 13:45:09 -0400

    wip

--- 11--Euler/euler-1.js
+++ 11--Euler/euler-1.js
@@ -1,9 +1,34 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
 /**
  *
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  *
  * Find the sum of all the multiples of 3 or 5 below 1000.
  *
- *
+ * Input:
+ * Output: Number, sum of all the multiples of 3 or 5 or below.
  */
 
+ // threeArray = multiples of 3
+ // fiveArray = multiples of 5
+ // results = holds the multples of both arays - any duplicates,
+ //           then sum
+ //
+ // forEach element of 3-array, loop through the 5-array elements
+ //
+ //
+ //
+ //
+var threeArray = [ ];
+
+function multiplyByThree() {
+  return 3;
+};
+
+describe('Function multiplyByThree', function(){
+  it('should be a function', function(){
+      assert.typeof(multiplyByThree, 'function');
+  });
+});
