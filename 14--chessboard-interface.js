72d79c7dc49a362df07c21f2096a073476639352
fab9 <rorou192@gmail.com>
2014-10-19 10:08:39 -0400

    begin testing the Chess constructor

--- 14--chessboard-interface.js
+++ 14--chessboard-interface.js
@@ -10,81 +10,73 @@
  * @method opening
  */
 function Chess(board) {
-  this.board = board;
+  this.board = board || "Default board";
 }
 
-Chess.prototype = {
-  constructor: Chess,
+// Chess.prototype = {
+//   constructor: Chess,
 
-  getPlayer: function(argument) {
-    // return ???
-  },
+//   getPlayer: function(argument) {
+//     // return ???
+//   },
 
-  display: function() {
-    var spacer = '+---+---+---+\n';
+//   display: function() {
+//     var spacer = '+---+---+---+\n';
 
-    return spacer +
-        // Apply `renderRow` to each `row` in `board`...
-        this.board.map(function renderRow(row){
-            return '| ' +
-                // Apply `renderCell` to each `cell` in `row`...
-                row.map(function renderCell(cell){
-                    // return 'X' if `cell` is TRUTHY otherwise return ' '
-                    return cell && 'X' || ' ';
-                }).join(' | ') // Place ' | ' between each `cell`...
-            + ' |\n';
-        }).join(spacer) // Place `spacer` between each `row`...
-    + spacer;
-  },
+//     return spacer +
+//         // Apply `renderRow` to each `row` in `board`...
+//         this.board.map(function renderRow(row){
+//             return '| ' +
+//                 // Apply `renderCell` to each `cell` in `row`...
+//                 row.map(function renderCell(cell){
+//                     // return 'X' if `cell` is TRUTHY otherwise return ' '
+//                     return cell && 'X' || ' ';
+//                 }).join(' | ') // Place ' | ' between each `cell`...
+//             + ' |\n';
+//         }).join(spacer) // Place `spacer` between each `row`...
+//     + spacer;
+//   },
 
-  move: function(piece, destination) {
-    // body...
-  },
+//   move: function(piece, destination) {
+//     // body...
+//   },
 
-  opening: function() {
-    // body...
-  }
-};
-
-
-
-
-/**
- * Function Definition: Position(x,y)
- * Represent a position on a chessboard with coordinates.
- * @type constructor
- * @property Number x coordinate
- * @property Number y coordinate
- */
-function Position(x, y) {
-  this.x = x;
-  this.y = y;
-}
-
-
-
-/**
- * Function Definition: Piece()
- * Represent a chesspiece on the board with name and color and appropriate starting position.
- * @type constructor
- * @param name
- * @param color
- * @method getName
- * @method getColor
- * @method setPosition(position)
- * @method toString
- */
-function Piece() {
-
-}
+//   opening: function() {
+//     // body...
+//   }
+// };
 
 
 
 
+// /**
+//  * Function Definition: Position(x,y)
+//  * Represent a position on a chessboard with coordinates.
+//  * @type constructor
+//  * @property Number x coordinate
+//  * @property Number y coordinate
+//  */
+// function Position(x, y) {
+//   this.x = x;
+//   this.y = y;
+// }
 
 
 
+// *
+//  * Function Definition: Piece()
+//  * Represent a chesspiece on the board with name and color and appropriate starting position.
+//  * @type constructor
+//  * @param name
+//  * @param color
+//  * @method getName
+//  * @method getColor
+//  * @method setPosition(position)
+//  * @method toString
 
+// function Piece() {
+
+// }
 
 
 
@@ -96,15 +88,21 @@ var chai = require('chai')
   , expect = chai.expect
   , should = chai.should();
 
-describe('Constructor functions', function () {
-  it('should be defined', function () {
-    assert.isDefined(Chess);
-    assert.isDefined(Piece);
-    assert.isDefined(Position);
-  });
-});
+// describe('Constructor functions', function () {
+//   it('should be defined', function () {
+//     assert.isDefined(Chess);
+//     assert.isDefined(Piece);
+//     assert.isDefined(Position);
+//   });
+// });
 
-it('setup board', function () {
-  ass
+// test suite for Chess object constructor
+describe('Chess', function () {
+  describe('constructor', function () {
+    it('should have a default board', function () {
+      var chess = new Chess();
+      expect(chess.board).to.equal("Default board");
+    });
+  });
 });
 
