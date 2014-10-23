ed19546fbff72739b0db87cfeb03d64ac4fb9593
fab9 <rorou192@gmail.com>
2014-10-23 16:31:55 -0400

    initial message

--- 14--lecture-notes.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect
-
-var ralph = { name: 'Ralph', gender: 'm'}
-var fabi = { name: 'Fabi', gender: 'f'}
-var ralph = { name: 'Dave', gender: 'm'}
-
-// see pics for rest of code
-//
