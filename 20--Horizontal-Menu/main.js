71f0ed6aa3265e2affdb531c2f657bb029a190df
fab9 <rorou192@gmail.com>
2014-10-29 07:51:48 -0400

    Horizontal menu: external js still not working

--- 20--Horizontal-Menu/main.js
+++ 20--Horizontal-Menu/main.js
@@ -1,14 +1,19 @@
+var menuItems = document.getElementById('nav').getElementsByClassName('nav-item');
+
 function addOpenClass(element){
   element.className = 'open';
+  // article.className = 'open';
 }
 
 function remOpenClass(element){
   element.className = '';
+  // article.className = '';
 }
+
 
 /**
  * @param HTMLElement element
- * @return Boolean whether `element` has `className` of "accordion-open"
+ * @return Boolean whether `element` has `className` of "open"
  */
 function hasOpenClass(element){
   if ( element.className === 'open' ){
@@ -18,10 +23,12 @@ function hasOpenClass(element){
   return false;
 }
 
+
 function toggleOpenClass(element){
   if ( hasOpenClass(element) ){
       return remOpenClass(element);
   }
 
   addOpenClass(element);
+
 }
